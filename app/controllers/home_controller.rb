45ef1b3f10c886457761aff269060f334ea89572
Dhaval Purohit <dhaval.purohit08@gmail.com>
2016-05-08 16:32:54 +0530

    adds search functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
 	def index
+		@word_field=params[:Word]
+		if !@word_field.blank?	
+			@word=Dictionary.find_by_Word(params[:Word])
+			@synonyms=@word.synonyms.split(",") if !@word.nil?
+			@antonyms=@word.antonyms.split(",") if !@word.nil?
+		end	
 	end		
 end
