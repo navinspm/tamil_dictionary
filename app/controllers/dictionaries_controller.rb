a7772a3b747c8f5eb8b55704abf517b48ff322ef
Dhaval Purohit <dhaval.purohit08@gmail.com>
2016-05-11 17:07:31 +0530

    adds word block

--- app/controllers/dictionaries_controller.rb
+++ app/controllers/dictionaries_controller.rb
@@ -7,6 +7,11 @@ class DictionariesController < ApplicationController
 			@word=Dictionary.find_by_Word(@word_field)
 			@synonyms=@word.synonyms.split(",") if !@word.nil?
 			@antonyms=@word.antonyms.split(",") if !@word.nil?
+		end
+		@word_ar=[]
+		30.times do
+			@word_day=Dictionary.random
+			@word_ar<<@word_day
 		end	
 	end		
 end
